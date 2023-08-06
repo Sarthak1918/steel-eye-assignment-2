25046a83c2bdd49511dc38f8f011948fa8e9f2fc
Sarthak1918 <pandasarthak12345@gmail.com>
2023-08-06 18:53:12 +0530

    Task-2 completed
    Task-2 completed

---
+++ index.js
@@ -0,0 +1,38 @@
+const main = document.querySelector("#main");
+const htmlContent = main.innerHTML;
+const plainText =  main.innerText;
+const plainTextPositions = [
+    {
+        start: 241,
+        end: 247,
+    },
+    {
+        start: 517,
+        end: 524,
+    },
+]
+
+main.innerHTML = highlightHTMLContent(htmlContent,plainText,plainTextPositions);
+
+function highlightHTMLContent(htmlContent, plainText, plainTextPositions) {
+    plainText = plainText.replace(/[\n\r]+|[\s]{2,}/g, ' ').replace(/  +/g, ' ').trim();
+    for (let pos of plainTextPositions) {
+        const word = plainText.slice(pos.start, pos.end);
+        let offsetIndex = getIndexOfTargetWord(htmlContent, word, pos.start);
+        if (offsetIndex === -1) continue;
+        htmlContent = htmlContent.slice(0, offsetIndex) + `<mark>${word}</mark>` + htmlContent.slice(offsetIndex + word.length);
+    }
+    return htmlContent;
+}
+
+
+function getIndexOfTargetWord(inputString, targetWord, start) {
+    let index = inputString.indexOf(targetWord);
+
+    while (index !== -1) {
+        if (index > start) break;
+        index = inputString.indexOf(targetWord, index + 1);
+    }
+
+    return index;
+}
